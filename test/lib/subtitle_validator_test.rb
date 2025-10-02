e1ca11c37364ca3b9de5a5364855034c83f0309d
Diogo Busanello <diogobusanello@gmail.com>
2025-10-02 16:15:20 +0100

    Fix Rubocop Omakase violation

--- test/lib/subtitle_validator_test.rb
+++ test/lib/subtitle_validator_test.rb
@@ -1,4 +1,4 @@
-require 'test_helper'
+require "test_helper"
 
 class SubtitleValidatorTest < ActiveSupport::TestCase
   VALID_SUBTITLE = <<~SUBTITLE
@@ -23,17 +23,17 @@ class SubtitleValidatorTest < ActiveSupport::TestCase
     That's right.
   SUBTITLE
 
-  test 'for valid subtitle returns empty array' do
+  test "for valid subtitle returns empty array" do
     skip
     assert_empty SubtitleValidator.new(VALID_SUBTITLE).errors
   end
 
-  test 'returns error messages when the subtitles contain errors' do
+  test "returns error messages when the subtitles contain errors" do
     skip
-    assert_equal ['Cue#1 is empty'], SubtitleValidator.new(BLANK_CUE).errors
+    assert_equal [ "Cue#1 is empty" ], SubtitleValidator.new(BLANK_CUE).errors
   end
 
-  test 'returns all error messages when the subtitles contain multiple errors' do
+  test "returns all error messages when the subtitles contain multiple errors" do
     skip
     two_empty_cues = <<~SUBTITLE
       1
@@ -43,18 +43,18 @@ class SubtitleValidatorTest < ActiveSupport::TestCase
       00:01:32,502 --> 00:01:40,971
     SUBTITLE
 
-    assert_equal ['Cue#1 is empty', 'Cue#2 is empty'], SubtitleValidator.new(two_empty_cues).errors
+    assert_equal [ "Cue#1 is empty", "Cue#2 is empty" ], SubtitleValidator.new(two_empty_cues).errors
   end
 
-  test '.validate! raises with errors when any errors exist' do
+  test ".validate! raises with errors when any errors exist" do
     skip
     e = assert_raises SubtitleValidator::ValidationError do
       SubtitleValidator.new(BLANK_CUE).validate!
     end
-    assert_equal 'Cue#1 is empty', e.message
+    assert_equal "Cue#1 is empty", e.message
   end
 
-  test 'returns errors for cues with more than 2 lines of text' do
+  test "returns errors for cues with more than 2 lines of text" do
     skip
     cue_2_has_three_lines = <<~SUBTITLE
       1
@@ -68,10 +68,10 @@ class SubtitleValidatorTest < ActiveSupport::TestCase
       That's right.
       Very.
     SUBTITLE
-    assert_equal ['Cue#2 has 3 or more lines'], SubtitleValidator.new(cue_2_has_three_lines).errors
+    assert_equal [ "Cue#2 has 3 or more lines" ], SubtitleValidator.new(cue_2_has_three_lines).errors
   end
 
-  test 'returns errors on invalid SRT structure' do
+  test "returns errors on invalid SRT structure" do
     skip
     cue_2_doesnt_start_with_index_digits = <<~SUBTITLE
       1
@@ -85,11 +85,11 @@ class SubtitleValidatorTest < ActiveSupport::TestCase
       That's right.
     SUBTITLE
 
-    assert_equal ['Failed to match group of lines 2 starting: SECOND'],
+    assert_equal [ "Failed to match group of lines 2 starting: SECOND" ],
                  SubtitleValidator.new(cue_2_doesnt_start_with_index_digits).errors
   end
 
-  test 'returns errors for cues with non incrementing timecodes per cue' do
+  test "returns errors for cues with non incrementing timecodes per cue" do
     skip
     cue_2_starts_before_cue_1_ends = <<~SUBTITLE
       1
@@ -102,18 +102,18 @@ class SubtitleValidatorTest < ActiveSupport::TestCase
       Royale with Cheese.
       That's right.
     SUBTITLE
-    assert_equal ['Cue#2 has a non-incrementing timecode'], SubtitleValidator.new(cue_2_starts_before_cue_1_ends).errors
+    assert_equal [ "Cue#2 has a non-incrementing timecode" ], SubtitleValidator.new(cue_2_starts_before_cue_1_ends).errors
   end
 
-  test 'returns errors if the final cue extends beyond the film duration' do
+  test "returns errors if the final cue extends beyond the film duration" do
     skip
-    assert_equal ['Subtitle duration (100.971) is longer than film duration (4 seconds)'],
+    assert_equal [ "Subtitle duration (100.971) is longer than film duration (4 seconds)" ],
                  SubtitleValidator.new(VALID_SUBTITLE, expected_film_duration: 4).errors
   end
 
-  test 'returns errors for cues with invalid timecodes' do
+  test "returns errors for cues with invalid timecodes" do
     skip
-    assert_equal ['Timecode 00:001:26,013 is badly formed'],
+    assert_equal [ "Timecode 00:001:26,013 is badly formed" ],
                  SubtitleValidator.new(
                    <<~SUBTITLE
                      1
@@ -121,9 +121,9 @@ class SubtitleValidatorTest < ActiveSupport::TestCase
                      foo
                    SUBTITLE
                  ).errors,
-                 'should check for errors in the minutes segment of the timecode'
+                 "should check for errors in the minutes segment of the timecode"
 
-    assert_equal ['Timecode 00:00:126,013 is badly formed'],
+    assert_equal [ "Timecode 00:00:126,013 is badly formed" ],
                  SubtitleValidator.new(
                    <<~SUBTITLE
                      1
@@ -131,9 +131,9 @@ class SubtitleValidatorTest < ActiveSupport::TestCase
                      foo
                    SUBTITLE
                  ).errors,
-                 'should check for errors in the seconds segment of the timecode'
+                 "should check for errors in the seconds segment of the timecode"
 
-    assert_equal ['Timecode 00:00:26,13 is badly formed'],
+    assert_equal [ "Timecode 00:00:26,13 is badly formed" ],
                  SubtitleValidator.new(
                    <<~SUBTITLE
                      1
@@ -141,9 +141,9 @@ class SubtitleValidatorTest < ActiveSupport::TestCase
                      foo
                    SUBTITLE
                  ).errors,
-                 'should check for errors in the milliseconds segment of the timecode'
+                 "should check for errors in the milliseconds segment of the timecode"
 
-    assert_equal ['Timecode 00:00: 26,013 is badly formed'],
+    assert_equal [ "Timecode 00:00: 26,013 is badly formed" ],
                  SubtitleValidator.new(
                    <<~SUBTITLE
                      1
@@ -151,10 +151,10 @@ class SubtitleValidatorTest < ActiveSupport::TestCase
                      foo
                    SUBTITLE
                  ).errors,
-                 'should not accept spaces in the middle of a timestamp'
+                 "should not accept spaces in the middle of a timestamp"
   end
 
-  test 'raises no errors for cues with supported HTML tags' do
+  test "raises no errors for cues with supported HTML tags" do
     skip
     subtitle_with_supported_html_tags = <<~SUBTITLE
       1
@@ -171,7 +171,7 @@ class SubtitleValidatorTest < ActiveSupport::TestCase
     assert_empty SubtitleValidator.new(subtitle_with_supported_html_tags).errors
   end
 
-  test 'returns errors for cues with unsupported HTML tags' do
+  test "returns errors for cues with unsupported HTML tags" do
     skip
     subtitle_with_unsupported_html_tags = <<~SUBTITLE
       1
@@ -185,10 +185,10 @@ class SubtitleValidatorTest < ActiveSupport::TestCase
       That's right.
     SUBTITLE
 
-    assert_equal ['Cue#1 contains invalid HTML tags'], SubtitleValidator.new(subtitle_with_unsupported_html_tags).errors
+    assert_equal [ "Cue#1 contains invalid HTML tags" ], SubtitleValidator.new(subtitle_with_unsupported_html_tags).errors
   end
 
-  test 'returns errors for cues with badly formed HTML like unclosed or unopened tags' do
+  test "returns errors for cues with badly formed HTML like unclosed or unopened tags" do
     skip
     subtitle_with_unsupported_html_tags = <<~SUBTITLE
       1
@@ -206,10 +206,10 @@ class SubtitleValidatorTest < ActiveSupport::TestCase
       What do they call a Big Mac?</i>
     SUBTITLE
 
-    assert_equal ['Cue#1 contains badly formed HTML', 'Cue#3 contains badly formed HTML'], SubtitleValidator.new(subtitle_with_unsupported_html_tags).errors
+    assert_equal [ "Cue#1 contains badly formed HTML", "Cue#3 contains badly formed HTML" ], SubtitleValidator.new(subtitle_with_unsupported_html_tags).errors
   end
 
-  test 'returns errors for cues with empty HTML tags' do
+  test "returns errors for cues with empty HTML tags" do
     skip
     subtitle_with_empty_html_tags = <<~SUBTITLE
       1
@@ -223,10 +223,10 @@ class SubtitleValidatorTest < ActiveSupport::TestCase
       That's right.
     SUBTITLE
 
-    assert_equal ['Cue#1 contains empty HTML tags'], SubtitleValidator.new(subtitle_with_empty_html_tags).errors
+    assert_equal [ "Cue#1 contains empty HTML tags" ], SubtitleValidator.new(subtitle_with_empty_html_tags).errors
   end
 
-  test 'returns errors for cues with HTML tags without content' do
+  test "returns errors for cues with HTML tags without content" do
     skip
     subtitle_with_contentless_html_tags = <<~SUBTITLE
       1
@@ -240,10 +240,10 @@ class SubtitleValidatorTest < ActiveSupport::TestCase
       That's right.
     SUBTITLE
 
-    assert_equal ['Cue#1 contains empty HTML tags'], SubtitleValidator.new(subtitle_with_contentless_html_tags).errors
+    assert_equal [ "Cue#1 contains empty HTML tags" ], SubtitleValidator.new(subtitle_with_contentless_html_tags).errors
   end
 
-  test 'returns errors for cues with a duration less than one frame' do
+  test "returns errors for cues with a duration less than one frame" do
     skip
     subtitle_with_short_cue = <<~SUBTITLE
       1
@@ -257,13 +257,13 @@ class SubtitleValidatorTest < ActiveSupport::TestCase
       That's right.
     SUBTITLE
 
-    assert_equal ['Cue#2 has a too short duration'], SubtitleValidator.new(subtitle_with_short_cue).errors
+    assert_equal [ "Cue#2 has a too short duration" ], SubtitleValidator.new(subtitle_with_short_cue).errors
   end
 
-  test 'returns an error message if the encoding is not UTF-8' do
+  test "returns an error message if the encoding is not UTF-8" do
     skip
     string_that_is_not_valid_utf8 = "\xFE\xFF\x00\x52"
-    assert_equal ["Invalid file encoding. UTF-8 required."],
+    assert_equal [ "Invalid file encoding. UTF-8 required." ],
                  SubtitleValidator.new(string_that_is_not_valid_utf8).errors
   end
 end
